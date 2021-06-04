f73ca3ce7e11179f8bf4e4bc75bcedaaad6972bc
jeevankumar-25 <26jeevankumar@gmail.com>
2021-06-05 04:02:52 +0530

    the cart list created function to add delete remove  is done

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -6,6 +6,7 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class CartService {
+  
 
   cartItems: CartItem[] = [];
 
@@ -79,5 +80,25 @@ export class CartService {
 
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
     console.log('----');
+  }
+
+  decrementQuantity (theCartItem: CartItem) {
+     theCartItem.quantity--;
+     if(theCartItem.quantity===0){
+       this.remove(theCartItem);
+     }
+     else{
+       this.computeCartTotals();
+     }
+  }
+  remove(theCartItem: CartItem) {
+   //get the index of item in the array
+   const itemIndex =this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id);
+   //if found,remove remove item from the array at the given index
+   if(itemIndex >-1){
+     this.cartItems.splice(itemIndex,1);
+     this.computeCartTotals();
+   }
+
   }
 }
