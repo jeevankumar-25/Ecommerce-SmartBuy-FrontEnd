e199f04576c50ec1343de2f821abe75b1a85bcb0
jeevankumar-25 <26jeevankumar@gmail.com>
2021-06-08 03:30:17 +0530

    Validation for checkout form and BehaviourSubject from observal is used

---
+++ src/app/validators/smart-buy-validators.spec.ts
@@ -0,0 +1,7 @@
+import { SmartBuyValidators } from './smart-buy-validators';
+
+describe('SmartBuyValidators', () => {
+  it('should create an instance', () => {
+    expect(new SmartBuyValidators()).toBeTruthy();
+  });
+});
