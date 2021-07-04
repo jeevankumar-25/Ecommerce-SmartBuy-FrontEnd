840ee10f13c8b659b87fa54c4a3ad35de05f252f
jeevankumar-25 <26jeevankumar@gmail.com>
2021-07-05 01:55:25 +0530

    The login/ logout using OKTA and new registration using OKTA

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId:'0oa157m2ckCzPqPHN5d7',
+        issuer:'https://dev-95847559.okta.com/oauth2/default',
+        redirectUri:'http://localhost:4200/login/callback',
+        scopes:['openid','profile','email']
+    }
+}
